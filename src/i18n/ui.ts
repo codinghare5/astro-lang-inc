7fbe534a17d1df0a8299b1d46f2c90a43682489a
Marcin Mukosiej <marcinmukosiej@gmail.com>
2023-11-24 12:30:38 +0100

    Translated descriptions across blog and tags.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,16 +13,24 @@ export const ui = {
     'nav.twitter': 'Twitter',
     'blog.description': 'All the articles posted so far...',
     'blog.languages': 'All the languages so far...',
-
+    'blog.tags.description': 'All the tags so far...',
+    'blog.tags.title': 'Tags',
   },
   fr: {
     'nav.home': 'Accueil',
     'nav.about': 'À propos',
+    'blog.description': "Tous les articles publiés jusqu'à présent...",
     'blog.languages': "Tous les langues publiés jusqu'à présent...",
+    'blog.tags.description': "Tous les tags utilisés jusqu'à présent...",
+    'blog.tags.title': "Tags",
+
   },
   pl: {
     'nav.about': 'O mnie',
+    'blog.description': "Wszystkie artukuły do tej pory...",
     'blog.languages': 'Wszystkie języki do tej pory...',
+    'blog.tags.description': "Wszystkie tagi do tej pory...",
+    'blog.tags.title': "Tagi",
   },
 } as const;
 
